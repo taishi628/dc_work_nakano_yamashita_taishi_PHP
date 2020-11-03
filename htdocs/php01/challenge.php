87d99e6e1d3ce08734f4a82093f57ae46bfaee4b
EC2 Default User <ec2-user@ip-172-31-23-46.ec2.internal>
2020-11-03 15:44:34 +0900

    1-4

---
+++ htdocs/php01/challenge.php
@@ -0,0 +1,30 @@
+<html lang="ja">
+<head>
+ <meta charset="UTF-8">
+ <title>†卍KADAI&emsp;1-7卍†</title>
+ <link rel="stylesheet" href="http://codecamp.lesson.codecamp.jp/sample/sample.css">
+</head>
+<body style="background-color:000">
+
+
+<h1 style="color:#8b0000"><?php print"やました&emsp;たいし"; ?></h1>
+<p style="color:#fff"><?php echo"初カキコ…ども…"; ?></p>
+<p style="color:#fff"><?php echo"俺みたいな無職で就職活動見据えてる腐れ野郎、他に、いますかっていねーか、はは"; ?></p>
+<p style="color:#fff"><?php echo"今日のクラスの会話"; ?><br>
+                    <?php echo"あの流行りの曲かっこいい　とか　あの服ほしい　とか"; ?><br>
+                    <?php echo"ま、それが普通ですわな"; ?></p>
+<p style="color:#fff"><?php echo"かたや俺は電子の砂漠で未来を見て、呟くんすわ"; ?><br>
+                    <?php echo"it’a true wolrd．狂ってる？それ、誉め言葉ね。"; ?></p>
+
+<p style="color:#fff"><?php echo"好きな音楽　eminem"; ?><br>
+                    <?php echo"尊敬する人間　アドルフ・ヒトラー（虐殺行為はNO）"; ?></p>
+
+<p style="color:#fff"><?php echo"なんつってる間に16時っすよ(笑)　あ～あ、ワークコースの辛いとこね、これな"; ?><br>
+                    <?php echo"ま、それが普通ですわな"; ?></p>
+
+<?php
+echo $alert = "<script type='text/javascript'>alert('俺はyamasita_taishiだぞ！！！！');</script>";?>
+
+
+</body>
+</html>
---
+++ htdocs/php01/hello.php
@@ -0,0 +1,21 @@
+<html lang="ja">
+<head>
+ <meta charset="UTF-8">
+ <title>はろぅ…わぁるど</title>
+ <link rel="stylesheet" href="http://codecamp.lesson.codecamp.jp/sample/sample.css">
+</head>
+<body style="background-color:000">
+
+
+<span style="color:#fff">
+<?php print"客人にPHP教材リンク集への行きかたを教えるためにクラウド９のターミナルで"; ?></span><br>
+
+<span style="color:#0F0">$ php -S 0.0.0.0:8000 -t ~/environment/htdocs</span><br>
+<span style="color:#fff">
+ <?php echo 'って打たせて'; ?> <br>
+ <?php echo "🎵🎶うぇるかむ　とぅ　あんだぁぐらうんど🎶🎵"; ?> <br>
+<?php echo 'と彼に小声で囁いた…'; ?> <br></span>
+
+
+</body>
+</html>
