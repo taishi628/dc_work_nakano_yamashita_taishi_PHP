779e492da079cf57e227f88773477f292e9310bf
EC2 Default User <ec2-user@ip-172-31-23-46.ec2.internal>
2020-12-04 14:29:33 +0900

    7-7

--- htdocs/php07/challenge_super_global.php
+++ htdocs/php07/challenge_super_global.php
@@ -5,8 +5,8 @@ $gender = '';
 $my_name = '';
 
 
-if (isset($_POST['gender']) === TRUE) {
-   $gender = htmlspecialchars($_POST['gender'], ENT_QUOTES, 'UTF-8');
+if (isset($_POST['gender']) === TRUE) {　//POSTで'ジェンダー'を返す
+   $gender = htmlspecialchars($_POST['gender'], ENT_QUOTES, 'UTF-8'); //ジェンダーを"''区別なくHTMLエンテイに変換
 }
 ?>
 
@@ -22,10 +22,12 @@ if (isset($_POST['gender']) === TRUE) {
    <p> あなたの性別は「<?php print $gender; ?>」です</p>
 <?php } ?>
 
+<ul>
 <li>お名前：<input id="my_name" type="text" name="my_name" value=""></li>
 <li>性別：<input type="radio" name="gender" value="man" <?php if ($gender === 'man') { print 'checked'; } ?>>男
 <input type="radio" name="gender" value="woman"<?php if ($gender === 'woman') { print 'checked'; } ?>>女</li>
 <li><input type="checkbox" name="mail" value="OK">お知らせメールを受け取る</li>
 <li><input type="button" name="button" value="送信"></li>
+</ul>
 
 </html>
