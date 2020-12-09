ff581ff1a583b12d8b442c777ebb136a8481c876
EC2 Default User <ec2-user@ip-172-31-23-46.ec2.internal>
2020-12-09 12:14:43 +0900

    7-7

--- htdocs/php07/challenge_super_global.php
+++ htdocs/php07/challenge_super_global.php
@@ -3,17 +3,21 @@
 // 変数初期化
 $name = '';
 $gender = '';
+$mail = '';
 
-if (isset($_POST['my_name'])===true) {
-   $name =  htmlspecialchars($_POST['my_name'], ENT_QUOTES, 'UTF-8');
-}
+   if (isset($_POST['my_name'])===true) {
+      $name =  htmlspecialchars($_POST['my_name'], ENT_QUOTES, 'UTF-8');
+   }
+   ?>
+<?php
+   if (isset($_POST['gender']) === TRUE) {//POSTで'ジェンダー'を返す
+      $gender = htmlspecialchars($_POST['gender'], ENT_QUOTES, 'UTF-8'); //ジェンダーを"''区別なくHTMLエンテイに変換
+   }
 ?>
 <?php
-if (isset($_POST['gender']) === TRUE) {//POSTで'ジェンダー'を返す
-   $gender = htmlspecialchars($_POST['gender'], ENT_QUOTES, 'UTF-8'); //ジェンダーを"''区別なくHTMLエンテイに変換
-}
-?>
-
+   if (isset($_POST['mail']) === TRUE) {
+      $mail= htmlspecialchars($_POST['mail'], ENT_QUOTES, 'UTF-8');
+   }?>
 
 
 <html lang="ja">
@@ -33,6 +37,8 @@ if (isset($_POST['gender']) === TRUE) {//POSTで'ジェンダー'を返す
    if ($gender === 'man' || $gender === 'woman') {?>
    <p> ここに選択した性別を表示：<?php print $gender; ?></p> 
 <?php } ?>
+
+
 <form method='post'>
 お名前： <input id="my_name" type="text" name='my_name' value=""> <br>　
 性別：<input type="radio" name='gender' value='man' <?php if ($gender === 'man'){ print 'checked'; }?>>男
