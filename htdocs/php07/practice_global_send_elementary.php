b823323febd6c2d2641534e72a1695e18819782e
EC2 Default User <ec2-user@ip-172-31-23-46.ec2.internal>
2020-12-10 15:04:05 +0900

    7-8

--- htdocs/php07/practice_global_send_elementary.php
+++ htdocs/php07/practice_global_send_elementary.php
@@ -8,10 +8,9 @@
 //と表示するプログラムを作成してください。
 //POSTでデータを送信するファイルと受信するファイルは、別々に作成してください。
 
-$name = ''; //変数初期化
+$name = '';//変数初期化
 if (isset($_POST['name']) === TRUE) {   //POSTで'ネーム'を返す
-   $name  = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');　//ネームを"''区別なくHTMLエンテイに変換
-
+   $name  = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');//ネーム変数を"''区別なくHTMLエンテイに変換
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -21,24 +20,15 @@ if (isset($_POST['name']) === TRUE) {   //POSTで'ネーム'を返す
 </head>
 <body>
    <h1>名前がいきわたるやつ</h1>
-   
-   <?php
-   // formから文字列が渡ってきている場合はnameさん表示
-   if (!isset($_POST['name']) || $_POST['name'] === '') {
-    $errors[] = 'Eメールアドレスが入力されていません';
-}
-   
-   
-   if (isset($name) === TRUE) {
-?>
-    <p>ようこそ<?php echo $name ?>さん</p>
-<?php } elseif(isset($name) === false) { ?>
+<?php//formから文字列が渡ってきている場合はnameさん表示
+   if (isset($_POST['name'])===true) {?>
+    <p>ようこそ<?php print $name;?>さん</p>
+<?php}else{ ?>
     <p>名前を入力してください</p>
-<?php } ?>
-   <!-- 名前いれるやつ -->
+<?php } ?><!-- 名前いれるやつ -->
    <form method="post">
-       <input type="text" name="name" value="<?php if (isset($name) === TRUE) { print $name; } ?>">
-       <input type="submit" value="送信">
+    <label>お名前： <input id="name" type="text" name='name' value="<?php print  htmlspecialchars($name, ENT_QUOTES, "UTF-8"); ?>"></label><br>
+   <label><input type="submit" name="button" value="送信"></label><br>
    </form>
 </body>
 </html>
