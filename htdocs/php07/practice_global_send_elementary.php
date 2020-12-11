0dd702ab7dee95d09b0de6f14830350a005ce742
EC2 Default User <ec2-user@ip-172-31-23-46.ec2.internal>
2020-12-11 14:25:04 +0900

    7-8

--- htdocs/php07/practice_global_send_elementary.php
+++ htdocs/php07/practice_global_send_elementary.php
@@ -8,10 +8,13 @@
 //と表示するプログラムを作成してください。
 //POSTでデータを送信するファイルと受信するファイルは、別々に作成してください。
 
-$name = '';//変数初期化
-if (isset($_POST['name']) === TRUE) {   //POSTで'ネーム'を返す
-   $name  = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');//ネーム変数を"''区別なくHTMLエンテイに変換
-?>
+$name = '';
+    //変数初期化
+if (isset($_POST['name']) === TRUE){
+    //POSTで'ネーム'を返す
+   $name  = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+    }   
+   ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -20,14 +23,15 @@ if (isset($_POST['name']) === TRUE) {   //POSTで'ネーム'を返す
 </head>
 <body>
    <h1>名前がいきわたるやつ</h1>
-<?php//formから文字列が渡ってきている場合はnameさん表示
-   if (isset($_POST['name'])===true) {?>
+   
+<?php 
+   if (isset($_POST['name']) === true) {?>
     <p>ようこそ<?php print $name;?>さん</p>
-<?php}else{ ?>
+<?php } else(isset($_POST['name']) === false) { ?>
     <p>名前を入力してください</p>
-<?php } ?><!-- 名前いれるやつ -->
+<?php } ?>
    <form method="post">
-    <label>お名前： <input id="name" type="text" name='name' value="<?php print  htmlspecialchars($name, ENT_QUOTES, "UTF-8"); ?>"></label><br>
+    <label>お名前： <input id="name" type="text" name='name' value="<?php print  htmlspecialchars($name, ENT_QUOTES, "UTF-8");?>"></label><br>
    <label><input type="submit" name="button" value="送信"></label><br>
    </form>
 </body>
