f4fa9b5a9d6e6956425fd3cbb795a75472d7574c
EC2 Default User <ec2-user@ip-172-31-23-46.ec2.internal>
2020-12-14 12:08:05 +0900

    7-9

--- htdocs/php07/practice_global_intermediate.php
+++ htdocs/php07/practice_global_intermediate.php
@@ -3,8 +3,10 @@ $jyanken = '';
     //変数初期化
 $gucyokipa = array();
     //配列を変数に代入
-$kettuka;
+$rand = '';
+$kettuka = false;
     //上記乱数と自分の選択と戦わせた結果
+    //いふ文が真前提だからデフォで偽にしてあげないと値が通らない
 
     if(isset($_POST['jyanken']) === TRUE){
     //じゃんけん変数の値が帰ってきている時に反映する
@@ -12,10 +14,9 @@ $kettuka;
     //配列を整備
    $jyanken  = htmlspecialchars($_POST['jyanken'], ENT_QUOTES, 'UTF-8');
    //じゃんけん変数を”’関係なくUTFで表示
-   $rand =　array_rand($jyanken);
-   //配列から乱数的に値を取り出す
-   
-   
+   $rand = $gucyokipa[array_rand($gucyokipa)];
+   //ランダ変数＝グーチョキパーの配列
+   //上記配列[]を乱数的に取り出す
    if ($jyanken === 'グー' && $rand === 'チョキ') {//勝負の結果を反映するIF文
         $kettuka = 'Win!!';//グー基準以下勝ちの定義
     }elseif ($jyanken === 'チョキ' && $rand === 'パー') {
@@ -54,9 +55,9 @@ $kettuka;
     <p>相手：<?php print $rand;?></p>
     <p>結果：<?php print $kettuka;?></p>
   <form method="post">
-     <label><input type="radio" name="jyanken" value="グー"<?php if ($gender === 'グー') { print 'checked'; }?>>グー</label>
-     <label><input type="radio" name="jyanken" value="チョキ"<?php if ($gender === 'チョキ') { print 'checked'; }?>>チョキ</label>
-     <label><input type="radio" name="jyanken" value="パー"<?php if ($gender === 'パー') { print 'checked'; }?>>パー</label>
+     <label><input type="radio" name="jyanken" value="グー"<?php if ($jyanken === 'グー') { print 'checked'; }?>>グー</label>
+     <label><input type="radio" name="jyanken" value="チョキ"<?php if ($jyanken === 'チョキ') { print 'checked'; }?>>チョキ</label>
+     <label><input type="radio" name="jyanken" value="パー"<?php if ($jyanken === 'パー') { print 'checked'; }?>>パー</label>
      <label><input type="submit" value="勝負!!"> </label>
    </form>
 </body>
