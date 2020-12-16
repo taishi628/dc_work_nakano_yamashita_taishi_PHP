3828313ddc619c53b1a8b6fb76d4ebe4ae91a06e
EC2 Default User <ec2-user@ip-172-31-23-46.ec2.internal>
2020-12-16 14:54:43 +0900

    8-5

--- htdocs/php07/practice_global_send_elementary.php
+++ htdocs/php07/practice_global_send_elementary.php
@@ -8,13 +8,13 @@
 //と表示するプログラムを作成してください。
 //POSTでデータを送信するファイルと受信するファイルは、別々に作成してください。
 
-$name = '';
-    //変数初期化
-if (isset($_POST['name']) === TRUE){
-    //POSTで'ネーム'を返す
-   $name  = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
-    }   
-   ?>
+    $name = '';
+        //変数初期化
+    if (isset($_POST['name']) === TRUE){
+        //POSTで'ネーム'を返す
+       $name  = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+        }   
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -22,17 +22,19 @@ if (isset($_POST['name']) === TRUE){
    <title>課題７－８</title>
 </head>
 <body>
-   <h1>名前がいきわたるやつ</h1>
-   
-<?php 
-   if(empty($_POST['name'])) {?>
-   <p>名前を入力してください</p>
-<?php } else{ ?>
-    <p>ようこそ<?php print $name;?>さん</p>
-<?php } ?>
-   <form method="post">
-    <label>お名前： <input id="name" type="text" name='name' value="<?php print  htmlspecialchars($name, ENT_QUOTES, "UTF-8");?>"></label><br>
-   <label><input type="submit" name="button" value="送信"></label><br>
-   </form>
+        
+    <h1>名前がいきわたるやつ</h1>
+       
+    <?php 
+       if(empty($_POST['name'])) {?>
+       <p>名前を入力してください</p>
+    <?php } else{ ?>
+        <p>ようこそ<?php print $name;?>さん</p>
+    <?php } ?>
+    <form method="post">
+        <label>お名前： <input id="name" type="text" name='name' value="<?php print  htmlspecialchars($name, ENT_QUOTES, "UTF-8");?>"></label><br>
+        <label><input type="submit" name="button" value="送信"></label><br>
+    </form>
+
 </body>
 </html>
